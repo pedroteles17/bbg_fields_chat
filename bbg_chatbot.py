42e955d8ae234ed72f63b4d0e43d4275e4ac2ab1
Pedro Teles <pedroteles1313@gmail.com>
2023-07-12 16:49:59 -0300

    do embedding

--- bbg_chatbot.py
+++ bbg_chatbot.py
@@ -1,24 +1,36 @@
 #%%
-import openai
+#import os
+
+#os.environ["OPENAI_API_KEY"] = "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"
+
+#from dotenv import load_dotenv
+#load_dotenv()
+
+#%%
 import pickle
 import pandas as pd
 
+from langchain.document_loaders import DataFrameLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
 #%%
 # Import data
-metadata = pickle.load(open('data/field_metadata.pickle', 'rb'))
+fields_metadata = pickle.load(open('data/field_metadata.pickle', 'rb'))
 
-docs = pickle.load(open('data/field_documentation.pkl', 'rb'))
+fields_documentation = pickle.load(open('data/field_documentation.pkl', 'rb'))
 
 # %%
 # Clean metadata 
-all_fields = []
-for field_group in metadata:
+all_fields_metadata = []
+for field_group in fields_metadata:
     for field in field_group['message']['element']['fieldResponse']:
         field_data = field['fieldData']
         field_info = field_data['fieldInfo']['fieldInfo']
 
         field_dict = {
-            'id': field_data['id'],
+            'source': field_data['id'],
             'mnemonic': field_info['mnemonic'],
             'description': field_info['description'],
             'data_type': field_info['datatype'],
@@ -28,34 +40,56 @@ for field_group in metadata:
             'ftype': field_info['ftype']
         }
 
-        all_fields.append(field_dict)
+        all_fields_metadata.append(field_dict)
 
-all_fields_df = pd.DataFrame(all_fields)
+all_fields_metadata_df = pd.DataFrame(all_fields_metadata)
 
 # %%
 # Clean documentation
-all_docs = []
-for field_group in docs:
+all_fields_documentation = []
+for field_group in fields_documentation:
     for field in field_group['message']['element']['fieldResponse']:
         field_data = field['fieldData']
         field_info = field_data['fieldInfo']['fieldInfo']
 
         field_dict = {
-            'id': field_data['id'],
+            'source': field_data['id'],
             'mnemonic': field_info['mnemonic'],
             'documentation': field_info['documentation']
         }
 
-        all_docs.append(field_dict)
+        all_fields_documentation.append(field_dict)
 
-all_docs_df = pd.DataFrame(all_docs)
+all_fields_documentation_df = pd.DataFrame(all_fields_documentation)
 
 # %%
 # Join documentation and metadata
-all_data = all_fields_df\
-    .merge(all_docs_df, on=['id', 'mnemonic'], how='left')\
+all_fields_data = all_fields_metadata_df\
+    .merge(all_fields_documentation_df, on=['source', 'mnemonic'], how='left')\
     .drop(columns=['property'])
 
+#%%
+# Load data using LangChain
+loader = DataFrameLoader(all_fields_data, page_content_column="documentation")
+
+documents = loader.load()
+
 # %%
-from langchain.llms import OpenAI
+# Split text into chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 100,
+    chunk_overlap  = 2,
+    length_function = len,
+    add_start_index = True,
+)
+
+docs = text_splitter.split_documents(documents)
+
 # %%
+# Create embeddings using OpenAI and FAISS
+embeddings = OpenAIEmbeddings()
+
+db = FAISS.from_documents(docs, embeddings)
+
+#%%
