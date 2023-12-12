852bfaac45ce3bfae52da6aafdeead35b9646b4f
Pedro Teles <pedroteles1313@gmail.com>
2023-12-11 23:24:40 -0300

    change file structure and delete old chatbot

--- data/clean_fields_data.py
+++ data/clean_fields_data.py
@@ -1,12 +1,13 @@
 #%%
 import pickle
 import pandas as pd
+import re
 
 #%%
 # Import data
-fields_metadata = pickle.load(open('data/field_metadata.pickle', 'rb'))
+fields_metadata = pickle.load(open('field_metadata.pickle', 'rb'))
 
-fields_documentation = pickle.load(open('data/field_documentation.pkl', 'rb'))
+fields_documentation = pickle.load(open('field_documentation.pkl', 'rb'))
 
 # %%
 # Clean metadata 
@@ -39,10 +40,14 @@ for field_group in fields_documentation:
         field_data = field['fieldData']
         field_info = field_data['fieldInfo']['fieldInfo']
 
+        # Replace newlines with spaces and remove extra spaces
+        field_documentation = field_info['documentation'].replace('\n', ' ')
+        field_documentation = re.sub(r'\s+', ' ', field_documentation)
+
         field_dict = {
             'source': field_data['id'],
             'mnemonic': field_info['mnemonic'],
-            'documentation': field_info['documentation']
+            'documentation': field_documentation
         }
 
         all_fields_documentation.append(field_dict)
@@ -53,8 +58,9 @@ all_fields_documentation_df = pd.DataFrame(all_fields_documentation)
 # Join documentation and metadata
 all_fields_data = all_fields_metadata_df\
     .merge(all_fields_documentation_df, on=['source', 'mnemonic'], how='left')\
-    .drop(columns=['property'])
+    .drop(columns=['property'])\
+    .dropna(subset=['documentation'])
 
-all_fields_data.to_excel('data/clean_fields_data.xlsx', index=False)
+all_fields_data.to_parquet('clean_fields_docs.parquet', index=False)
 
 #%%
